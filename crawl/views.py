14088cb428b4c015b7455aabc9a650424d0edc69
kaevowen <kaev.owen@gmail.com>
2020-03-02 20:30:36 +0900

    meh

--- crawl/views.py
+++ crawl/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup as bs
+import requests
+import xml.etree.ElementTree as elemTree
 
 def wuhan_crawl(req):
-    return render(req, 'ㅇㅇ')
+    client_id = '0ZH4Jxq2Sil2c_0uNWm6'
+    client_secret = 'PM4PbNyF68'
+    url = 'https://openapi.naver.com/v1/search/news.xml?query=' + '우한'
+    headers = {"X-Naver-Client-Id": client_id, "X-Naver-Client-Secret" : client_secret}
+    req = bs(requests.get(url, headers=headers).content, 'lxml')
+
+    lst = []
+    for r in req.find_all('title'):
+        lst.append(r.text)
+
+    return render(req, 'c/crawl_result.html', lst)
+
 # Create your views here.
+
+wuhan_crawl()
